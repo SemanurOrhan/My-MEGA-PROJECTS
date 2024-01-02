ab6d55e5a46d1182018347364476e7640325fce7
Semanur <119792917+SemanurOrhan@users.noreply.github.com>
2024-01-02 13:37:44 +0300

    Add files via upload

---
+++ src/App/Footer.jsx
@@ -0,0 +1,8 @@
+const Footer = ({ Footer }) => {
+    return (
+       <div className="footer" style={{border:"1px ridge lightblue" }}>
+           <h3 style={{color: "steelblue"}}>Kişisel Bilgiler içerir. Her hakkı saklıdır.</h3>
+       </div>
+   );
+}
+export default Footer;
---
+++ src/App/Header.jsx
@@ -0,0 +1,19 @@
+const Header = ({ Header }) => {
+    return (
+       <div className="header" style={{backgroundColor: "yellowgreen" }}>
+           <h2>Ben Kimim?</h2>
+       </div>
+   );
+}
+
+
+const icerik =()=>{
+    return (
+        <>
+        <p>Merhaba</p>
+        </>
+
+    );
+}
+
+export default Header ; icerik;
