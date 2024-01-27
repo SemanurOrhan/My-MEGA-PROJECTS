6174fddc01fd63e71ec8db2ed57c395de7011c44
Semanur <119792917+SemanurOrhan@users.noreply.github.com>
2024-01-27 22:50:15 +0300

    Final Project

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,27 @@
-// App.js
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import { AuthProvider } from './Auth';
+import Home from './components/Home';
+import About from './components/About';
 import Navbar from './components/Navbar';
-import Home from './pages/Home.js';
-import Products from './pages/Products';
-import ProductDetail from './pages/ProductDetail';
-import CategoryDetail from './pages/CategoryDetail';
-import Favorites from './pages/Favorites';
-import Cart from './pages/Cart';
-import PrivateRoute from './components/PrivateRoute';
+import './App.css';
 
-const App = () => {
+
+function App() {
+  const [count, setCount] = useState(0);
+
   return (
-    <AuthProvider>
+    <>
       <Router>
-        <div>
-          <Navbar />
-
-          <Switch>
-            <Route path="/" exact component={Home} />
-            <Route path="/products" exact component={Products} />
-            <Route path="/products/:id" component={ProductDetail} />
-            <Route path="/categories/:id" component={CategoryDetail} />
-            
-            {/* PrivateRoute kullanarak favori sayfasını koruma */}
-            <PrivateRoute path="/favorites" component={Favorites} />
-
-            <Route path="/cart" component={Cart} />
-          </Switch>
-        </div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          
+          <Route path="/" element={<Navbar />} />
+        </Routes>
       </Router>
-    </AuthProvider>
+    </>
   );
-};
+}
 
 export default App;
---
+++ src/components/About.jsx
@@ -0,0 +1,14 @@
+
+
+import React from 'react';
+
+const About = () => {
+  return (
+    <div>
+      <h2>About Us</h2>
+      <p>Diğer içerikler</p>
+    </div>
+  );
+};
+
+export default About;
