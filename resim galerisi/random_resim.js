650ec8b4e0720e4d820cc38a8be342a7e2f825a3
Semanur <119792917+SemanurOrhan@users.noreply.github.com>
2024-02-14 21:51:45 +0300

    Delete resim galerisi directory

--- resim galerisi/random_resim.js
+++
@@ -1,8 +0,0 @@
-function newImages() {
-    const images = document.querySelectorAll('.rsm1');
-    images.forEach((img, index) => {
-        img.src = `https://source.unsplash.com/random/300x300 =${index + 1}`;
-    });
-}
-
-
